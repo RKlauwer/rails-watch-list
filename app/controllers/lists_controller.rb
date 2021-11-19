9b63ed70d40aece7cf515ac707ba5f1f2b0c5438
Raymond Klauwer <klauwerraymond@gmail.com>
2021-11-19 11:52:48 -0600

    challange part done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,28 @@
-class ListController < ApplicationController
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(lists_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  private
+
+  def lists_params
+    params.require(:list).permit(:name)
+  end
 end
